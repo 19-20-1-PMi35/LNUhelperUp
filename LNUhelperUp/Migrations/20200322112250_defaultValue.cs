5b29507f96054cb9096e9df504cfeb00d8546b34
Peremoha <dvtytrhenbqrjvg@gmail.com>
2020-03-22 19:51:40 +0200

    add datainitializer

---
+++ LNUhelperUp/Migrations/20200322112250_defaultValue.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LNUhelperUp.Migrations
+{
+    public partial class defaultValue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Faculties",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Біологічний" },
+                    { 19, "Юридичний" },
+                    { 18, "Хімічний" },
+                    { 17, "Філософський" },
+                    { 16, "Філологічний" },
+                    { 15, "Фізичний" },
+                    { 14, "Управління фінансами та бізнесу" },
+                    { 13, "Прикладної математики та інформатики" },
+                    { 12, "Педагогічної освіти" },
+                    { 11, "Міжнародних відносин" },
+                    { 9, "Культури та мистецтв" },
+                    { 8, "Історичний" },
+                    { 7, "Іноземних мов" },
+                    { 6, "Фурналістики" },
+                    { 5, "Електроніки та комп'ютерних технологій" },
+                    { 4, "Економічний" },
+                    { 3, "Геологічний" },
+                    { 2, "Географічний" },
+                    { 10, "Механіко-математичний факультет" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Admin" },
+                    { 2, "User" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Faculties",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
--- LNUhelperUp/Models/Configurations/FacultyConfiguration.cs
+++ LNUhelperUp/Models/Configurations/FacultyConfiguration.cs
@@ -7,29 +7,53 @@ using System.Threading.Tasks;
 
 namespace LNUhelperUp.Models.Configurations
 {
-	public class FacultyConfiguration : IEntityTypeConfiguration<Faculty>
-	{
-		public void Configure(EntityTypeBuilder<Faculty> builder)
-		{
-			builder.ToTable("Faculties");
+    public class FacultyConfiguration : IEntityTypeConfiguration<Faculty>
+    {
+        public void Configure(EntityTypeBuilder<Faculty> builder)
+        {
+            builder.ToTable("Faculties");
 
-			builder.HasKey(p => p.Id);
+            builder.HasKey(p => p.Id);
 
-			builder.HasMany(c => c.Users)
-				.WithOne(c => c.Faculty)
-				.HasForeignKey(c => c.FacultyId)
-				.OnDelete(DeleteBehavior.Cascade);
+            builder.HasMany(c => c.Users)
+                .WithOne(c => c.Faculty)
+                .HasForeignKey(c => c.FacultyId)
+                .OnDelete(DeleteBehavior.Cascade);
 
-			builder.HasMany(c => c.Announcements)
-				.WithOne(c => c.Faculty)
-				.HasForeignKey(c => c.UserId)
-				.OnDelete(DeleteBehavior.Cascade);
+            builder.HasMany(c => c.Announcements)
+                .WithOne(c => c.Faculty)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
 
-			builder.HasMany(c => c.Events)
-				.WithOne(c => c.Faculty)
-				.HasForeignKey(c => c.UserId)
-				.OnDelete(DeleteBehavior.Cascade);
+            builder.HasMany(c => c.Events)
+                .WithOne(c => c.Faculty)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
 
-		}
-	}
+            builder.HasData(
+                new Faculty[]
+                {
+                    new Faculty {Id = 1, Name = "Біологічний"},
+                    new Faculty {Id = 2, Name = "Географічний"},
+                    new Faculty {Id = 3, Name = "Геологічний"},
+                    new Faculty {Id = 4, Name = "Економічний"},
+                    new Faculty {Id = 5, Name = "Електроніки та комп'ютерних технологій"},
+                    new Faculty {Id = 6, Name = "Фурналістики"},
+                    new Faculty {Id = 7, Name = "Іноземних мов"},
+                    new Faculty {Id = 8, Name = "Історичний"},
+                    new Faculty {Id = 9, Name = "Культури та мистецтв"},
+                    new Faculty {Id = 10, Name = "Механіко-математичний факультет"},
+                    new Faculty {Id = 11, Name = "Міжнародних відносин"},
+                    new Faculty {Id = 12, Name = "Педагогічної освіти"},
+                    new Faculty {Id = 13, Name = "Прикладної математики та інформатики"},
+                    new Faculty {Id = 14, Name = "Управління фінансами та бізнесу"},
+                    new Faculty {Id = 15, Name = "Фізичний"},
+                    new Faculty {Id = 16, Name = "Філологічний"},
+                    new Faculty {Id = 17, Name = "Філософський"},
+                    new Faculty {Id = 18, Name = "Хімічний"},
+                    new Faculty {Id = 19, Name = "Юридичний"}
+                }
+                );
+        }
+    }
 }
