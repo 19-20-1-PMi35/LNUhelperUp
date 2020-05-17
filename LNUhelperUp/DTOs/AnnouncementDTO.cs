fe69c649fb079c8172d6fb795c889207f27663d1
Peremoha <dvtytrhenbqrjvg@gmail.com>
2020-05-17 22:33:27 +0300

    add some DTOs

---
+++ LNUhelperUp/DTOs/AnnouncementDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LNUhelperUp.DTOs
+{
+    public class AnnouncementDTO
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime CreateAt { get; set; }
+        public int UserId { get; set; }
+        public int FacultyId { get; set; }
+    }
+}
